7654175b68222f93fbcd781c3714a1e61c9821bb
Dwarven shpere Wyl waynwood <dwyl-way@mi-o6.21-school.ru>
2019-06-15 22:11:22 +0300

    upd 20190615

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,56 @@
 /*   By: dwyl-way <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 13:46:05 by dwyl-way          #+#    #+#             */
-/*   Updated: 2019/06/01 17:51:07 by dwyl-way         ###   ########.fr       */
+/*   Updated: 2019/06/15 21:51:01 by dwyl-way         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
+static char	*get_cur_line(char **str)
 {
-	char    buf[BUFF_SIZE + 1];
-	int		ret;
+	char	*line;
+	char	*str_temp;
+	int		len;
 
+	len = 0;
+	str_temp = NULL;
 	line = NULL;
-	ret = 0;
+	while (str[0][len] != '\n' && str[0][len] != '\0')
+		len++;
+	if (ft_strlen(*str))
+		line = ft_strsub(*str, 0, len);
+	if (str[0][len] != '\0')
+		str_temp = ft_strdup(&str[0][len + 1]);
+	ft_strdel(str);
+	*str = str_temp;
+	return (line);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*str;
+	char		buf[BUFF_SIZE + 1];
+	int			ret;
+	char		*temp;
+
+	if (fd < 0 || !line || read(fd, buf, 0) < 0)
+		return (-1);
+	if (!str)
+		str = ft_strnew(0);
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		ft_putstr(buf);
+		temp = ft_strjoin(str, buf);
+		ft_strdel(&str);
+		str = ft_strdup(temp);
+		ft_strdel(&temp);
+		if (ft_strchr(str, '\n') && (*line = get_cur_line(&str)))
+			return (1);
 	}
-
-	return (fd);
+	if (ret == -1)
+		return (-1);
+	if ((ft_strlen(str)) && (*line = get_cur_line(&str)))
+		return (1);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: dwyl-way <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 13:46:27 by dwyl-way          #+#    #+#             */
-/*   Updated: 2019/06/01 17:47:55 by dwyl-way         ###   ########.fr       */
+/*   Updated: 2019/06/15 21:28:42 by dwyl-way         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 9999
 # include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
